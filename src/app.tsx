13ac9e1942e35656849f06c1d0b0e50ac322bcd0
solidSpoon <cedarrival@outlook.com>
2024-02-03 13:07:49 +0800

    tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,6 @@
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(<h1 className="text-3xl font-bold underline">
+    Hello world!
+</h1>);
