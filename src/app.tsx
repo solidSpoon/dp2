936e100e46ca94fec24b4cb66c8d16a08b5b546e
solidSpoon <cedarrival@outlook.com>
2024-02-04 14:59:29 +0800

    alias

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,16 @@
 import {createRoot} from 'react-dom/client';
+import a from "@/util/a";
+
+const App = () => {
+
+    a();
+
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
+}
 
 const root = createRoot(document.body);
-root.render(<h1 className="text-3xl font-bold underline">
-    Hello world!
-</h1>);
+root.render(<App/>);
